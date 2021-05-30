bcc0354f46fdd10c596ef011c264f2abb254539b
AlfredoGV77 <jgutierrezv1901@alumno.ipn.mx>
2021-05-30 01:10:38 -0500

    3 graficas

--- Gráficas de Proyecto Aula(Individual)/grafica.py
+++ Gráficas de Proyecto Aula(Individual)/grafica.py
@@ -1,77 +1,132 @@
 import pandas as pd
 import webbrowser
+
+
+
+#PARA EL PRIMER CSV------------------------------------------------------------------------------------------------------------------------------
 datos = pd.read_csv("PrevalenciaC.csv")
 df = pd.DataFrame(datos)
-i=0
 script = ""
-while i<11:
-        script = script+"{x:'"+str(datos['Estado'].loc[i])+"', y:"+str(datos['Prevalencia consumo de cigarro en 30 dias'].loc[i])+", z:"+str(i+1)+"},\n"
+i=0
+while i<31:
+        script = script+"{x:'"+datos['Estado'].loc[i]+"', y:"+str(datos['Prevalencia consumo de cigarro en 30 dias'].loc[i])+", z:"+str(i+1)+"},\n"
         i+=1
 print (script)
-df.groupby('Estado')['Prevalencia consumo de cigarro en 30 dias'].sum().plot(kind='bar',legend='Reverse',title='Cigarros Fumados por Dia ',color='cyan')
-#df.groupby('Estado')['Prevalencia consumo de cigarro en 30 dias'].sum().plot(kind='bar',legend='Reverse')
-#df.groupby('Estado')['Prevalencia consumo de cigarro en 30 dias'].sum().plot(kind='barh',legend='Reverse')
-#df.groupby('Estado')['Prevalencia consumo de cigarro en 30 dias'].sum().plot(kind='hist',legend='Reverse')
-#df.groupby('Estado')['Prevalencia consumo de cigarro en 30 dias'].sum().plot(kind='kde',legend='Reverse')
-#df.groupby('Estado')['Prevalencia consumo de cigarro en 30 dias'].sum().plot(kind='density',legend='Reverse')
-#df.groupby('Estado')['Prevalencia consumo de cigarro en 30 dias'].sum().plot(kind='pie',legend='Reverse')
+df.groupby('Estado')['Prevalencia consumo de cigarro en 30 dias'].sum().plot(kind='line',title='Transacciones',color='gray')
 
 
-f = open('camaras.html','w')
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-"""
 
-{x:'CDMX', y:28, z:1},
-{x:'Puebla', y:27, z:2},
-{x:'Edo.Mexico', y:27, z:3},
-{x:'Guanajuato', y:23, z:4},
-{x:'Durango', y:24, z:5},
-{x:'Morelos', y:22, z:6},
-{x:'Zacatecas', y:22, z:7},
-{x:'San Luis Potosi', y:20, z:8},
-{x:'Sonora', y:20, z:9},
-{x:'Guerrero', y:20, z:10},
-{x:'Hidalgo', y:19, z:11},
+#PARA EL SEGUNDO CSV-----------------------------------------------------------------------------------------------------------------
+datos2 = pd.read_csv("AdiccionesDatos.csv")
+df1 = pd.DataFrame(datos2)
+script2 = ""
+i2=0
+while i2<13:
+        script2 = script2+"{x:'"+str(datos2['Ano'].loc[i2])+"', y:"+str(datos2['Alcohol'].loc[i2])+", z:"+str(i2+1)+"},\n"
+        i2+=1
+print (script2)
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 """
+#PARA EL 3ER CSV----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+datos3=pd.read_csv("AbusoSustancias.csv")
+df2 = pd.DataFrame(datos3)
+script3 = ""
+i3=0
+while i3<5:
+        script3 = script3+"{value:'"+str(datos3['Pacientes'].loc[i3])+"', label:"+str(datos3['Enfermedad'].loc[i3])+", z:"+str(i3+1)+"},\n"
+        i3+=1
+print (script3)
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+"""
 
 
 
+#GRAFICAS XD----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#df1.groupby('Ano')['Alcohol'].sum().plot(kind='bar',legend='Reverse')
+#df1.groupby('Name')['Transaction'].sum().plot(kind='barh',legend='Reverse')
+#df.groupby('Model')['Price'].sum().plot(kind='hist',legend='Reverse')
+#df1.groupby('Ano')['Alcohol'].sum().plot(kind='box',legend='Reverse')
+#df1.groupby('Name')['Transaction'].sum().plot(kind='kde',legend='Reverse')
+#df1.groupby('Name')['Transaction'].sum().plot(kind='density',legend='Reverse')
+#df1.groupby('Name')['Transaction'].sum().plot(kind='area',legend='Reverse')
+#df.groupby('Model')['Price'].sum().plot(kind='pie',legend='Reverse')
+#df.groupby('Model')['Price'].sum().plot(kind='scatter',legend='Reverse')
+#df1.groupby('Name')['Transaction'].sum().plot(kind='hexbin',legend='Reverse')
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-
-
-f = open('Adicciones.html','w')
+f = open('Adicciones2.html','w')
 
 mensaje = """<html>
 <head>
-<title>Gráficas de Adicciones</title>
+<title>Gráficas con JavaScript</title>
 <meta charset='utf-8'>
 <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>
 <link rel='stylesheet' type='text/css' href='morris.css'>  
-<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js'></script>
-<script src='http://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js'></script>
-<script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js' integrity='sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM' crossorigin='anonymous'></script> 
-<script src='morris.min.js'></script>
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
+<script src='Morris.js'></script>
 <script>    
 function grafica1(){
-  new Morris.Bar({
+  new Morris.Line({
   element: 'graph',
   data: ["""+script+"""
           ],
   xkey: 'x',
-  ykeys: ['y'],
+  ykeys: ['y','z'],
   axes:false,
-  labels: ['Prevalencia:'],
+  labels: ['Y','Z'],
   resize:true,
   lineColors:['#2CB4AC']
   });
 }
+
+function grafica2(){
+  new Morris.Area({
+  element: 'graph',
+  data: ["""+script2+"""
+          ],
+  xkey: 'x',
+  ykeys: ['y','z'],
+  axes:false,
+  labels: ['Y','Z'],
+  resize:true,
+  lineColors:['#2CB4AC']
+  });   
+}
+
+
+function grafica3(){
+  new Morris.Donout({
+  element: 'graph',
+  data: ["""+script2+"""
+          ],
+  xkey: 'x',
+  ykeys: ['y','z'],
+  axes:false,
+  labels: ['Y','Z'],
+  resize:true,
+  lineColors:['#2CB4AC']
+  });   
+}
+
+
+
 </script>
 </head>
 <body>
 <div id='graph'></div>
+<input type='button' value='grafica' onclick='grafica1()'>
+<input type='button' value='grafica' onclick='grafica2()'>
+<input type='button' value='grafica' onclick='grafica1()'>
+<input type='button' value='grafica' onclick='grafica1()'>
+<input type='button' value='grafica' onclick='grafica1()'>
 <input type='button' value='grafica' onclick='grafica1()'>
 </body>
 </html>"""
@@ -79,3 +134,5 @@ f.write(mensaje)
 f.close()
 
 webbrowser.open_new_tab('Adicciones.html')
+
+#print(datos.to_excel("CreditCards.xls",sheet_name="datos"))
